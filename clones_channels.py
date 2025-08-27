23161b9b17071c9c155484d556061cc6cb5e8beb
neuralXray <169923996+neuralXray@users.noreply.github.com>
2025-08-27 22:29:43 +0200

    Update clone(s) detection of IRCCloud users.
    All of them now use a new generic ident. And, as they're in a bounzer,
    they share the same IP. Therefore, it's impossible to find their clones.

--- clones_channels.py
+++ clones_channels.py
@@ -29,42 +29,36 @@ def whois_name_line(word, word_eol, userdata):
     channel_history = []
     clones_nicks = []
     clones = []
-    if ip.endswith('.79j.0Ar7OI.virtual') & (chat == 'irc.chathispano.com'):
-        match_case = 0
-    else:
-        match_case = 1
-
-    for chan in hexchat.get_list('channels'):
-        if (chan.id == connection_id) & (chan.type == 2):
-            user_list = chan.context.get_list('users')
-            for user in user_list:
-                nick_check = user.nick
-                nick_host = user.host
-
-                if nick_check == nick:
-                    channel = user.prefix + chan.channel
-                    channel_history.append(channel)
-
-                if nick_host:
-                    nick_ident = nick_host[:nick_host.find('@')]
-                    nick_ip = nick_host[nick_host.find('@') + 1:]
-                    if match_case == 0:
-                        match = (nick_ip == ip) & (nick_ident == ident)
-                    else:
-                        match = nick_ip == ip
-                    if (nick_check != nick) & (nick_check not in clones_nicks) & match:
-                        clones_nicks.append(nick_check)
-                        if is_colored_nicks_loaded:
-                            nick_check = colored_nick(nick_check)
-                        clones.append(nick_check + '!' + nick_ident)
-
     if is_colored_nicks_loaded:
         col_nick = colored_nick(nick)
     else:
         col_nick = nick
-    printout = '*\t[' + col_nick + '] ' + str(len(clones)) + ' clone(s)'
-    if clones:
-        printout = printout + ': ' + ', '.join(clones)
+
+    if ip.endswith('.79j.0Ar7OI.virtual') and (chat == 'irc.chathispano.com'):
+        printout = '*\t[' + col_nick + '] Unable to find clone(s) (IRCCloud)'
+    else:
+        for chan in hexchat.get_list('channels'):
+            if (chan.id == connection_id) and (chan.type == 2):
+                user_list = chan.context.get_list('users')
+                for user in user_list:
+                    nick_check = user.nick
+                    nick_host = user.host
+
+                    if nick_check == nick:
+                        channel = user.prefix + chan.channel
+                        channel_history.append(channel)
+
+                    if nick_host:
+                        nick_ident = nick_host[:nick_host.find('@')]
+                        nick_ip = nick_host[nick_host.find('@') + 1:]
+                        if (nick_ip == ip) and (nick_check != nick) and (nick_check not in clones_nicks):
+                            clones_nicks.append(nick_check)
+                            if is_colored_nicks_loaded:
+                                nick_check = colored_nick(nick_check)
+                            clones.append(nick_check + '!' + nick_ident)
+        printout = '*\t[' + col_nick + '] ' + str(len(clones)) + ' clone(s)'
+        if clones:
+            printout = printout + ': ' + ', '.join(clones)
     if is_colored_nicks_loaded:
         context = server_context(connection_id)
         context.prnt(printout)
